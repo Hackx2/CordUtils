c0dd48ad49db5e6bc6c9ebd4bf52bbdbd00d7b74
HackDev <80620538+Hackx2@users.noreply.github.com>
2025-02-16 03:24:28 +0000

    blah

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => (
     <footer className="text-white mt-o">
         <div className="container mx-auto text-center">
             <a
-                href="https://github.com/hackx2/CordUtils"
+                href="https://github.com/hackx2/CordUtils-Website"
                 className="mt-2 inline-block"
             >
                 <Github className="text-white hover:text-gray-400" size={24} />
