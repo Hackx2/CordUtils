04adae1d8293e0f4027fb84775eeba25b904ba2c
HackDev <80620538+Hackx2@users.noreply.github.com>
2025-02-16 16:35:09 +0000

    a

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,5 +1,9 @@
-export default ({ children }: { children: React.ReactNode }) => (
-    <main className="flex flex-col gap-2 w-full max-w-md p-6 rounded-xl shadow-lg backdrop-blur-md border border-gray-700">
-        {children}
-    </main>
-);;
+import React from "react";
+
+const Container: React.FC<{ children: React.ReactNode }> = ({ children }) => (
+  <main className="flex flex-col gap-2 w-full max-w-md p-6 rounded-xl shadow-lg backdrop-blur-md border border-gray-700">
+    {children}
+  </main>
+);
+
+export default Container;
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,13 @@
 import { Github } from 'lucide-react';
 
-export default () => (
-    <footer className="text-white mt-o">
-        <div className="container mx-auto text-center">
-            <a href="https://github.com/hackx2/CordUtils-Website" className="mt-2 inline-block">
-                <Github className="text-white hover:text-gray-400" size={24} />
-            </a>
-        </div>
-    </footer>
+const Footer: React.FC = () => (
+  <footer className="text-white mt-0">
+    <div className="container mx-auto text-center">
+      <a href="https://github.com/hackx2/CordUtils-Website" className="mt-2 inline-block">
+        <Github className="text-white hover:text-gray-400" size={24} />
+      </a>
+    </div>
+  </footer>
 );
+
+export default Footer;
