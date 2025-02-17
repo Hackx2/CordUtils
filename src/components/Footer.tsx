4bdf66ecbf3aa026ff52c37684c8a83eebfdaae9
HackDev <80620538+Hackx2@users.noreply.github.com>
2025-02-17 03:31:15 +0000

    update repo link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { Github } from 'lucide-react';
 const Footer: React.FC = () => (
   <footer className="text-white mt-0">
     <div className="container mx-auto text-center">
-      <a href="https://github.com/hackx2/CordUtils-Website" className="mt-2 inline-block">
+      <a href="https://github.com/hackx2/CordUtils" className="mt-2 inline-block">
         <Github className="text-white hover:text-gray-400" size={24} />
       </a>
     </div>
