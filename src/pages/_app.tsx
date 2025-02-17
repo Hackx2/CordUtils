70563c7e90fe4acf5cb378f61c91349f0e3af080
HackDev <80620538+Hackx2@users.noreply.github.com>
2025-02-17 22:21:03 +0000

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,11 @@
-// css???
 import "@/styles/globals.css";
-
-// Types
-import type { AppProps } from "next/app";
-
-// Components
 import Header from "@/components/Header";
-
-// la sexy window manager
-import { preventEvent, backgroundMovement } from "@/utils/windowManager";
+import type { AppProps } from "next/app";
+import { PreventEvent, BackgroundMovement } from "@/utils/windowManager";
 
 export default function App({ Component, pageProps }: AppProps) {
-  backgroundMovement();
-  preventEvent('contextmenu');
+  BackgroundMovement();
+  PreventEvent('contextmenu');
 
-  return (<><Header/><Component {...pageProps} /></>);
+  return (<><Header /><Component {...pageProps} /></>);
 }
