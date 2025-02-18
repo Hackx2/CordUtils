4eaffcd8cf3bf8028a529dca1d39b494d46a49de
HackDev <80620538+Hackx2@users.noreply.github.com>
2025-02-18 02:49:44 +0000

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,6 @@ import { OpenWindowLink } from "@/utils/windowManager";
 
 export default function PageIndex() {// owo
   OpenWindowLink('/webhook/delete');// homescreen when?!?!????!
-
+  console.log(`why u here?!?!?!?`);
   return null;
 }
